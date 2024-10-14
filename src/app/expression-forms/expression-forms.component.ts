cb31d2b423d4df095800f46694fdc86a902b6c3e
alopezo <alopez72@gmail.com>
2024-10-14 15:27:06 -0300

    Changing expression formatting

--- src/app/expression-forms/expression-forms.component.ts
+++ src/app/expression-forms/expression-forms.component.ts
@@ -19,13 +19,25 @@ export class ExpressionFormsComponent implements OnInit {
   }
 
   getSafeHtml(input: string) {
-    let transformed = new ScgHighlightingPipe().transform(input);
-    // transformed = transformed.replace(/,/g, ',\n');
-    transformed = transformed.replace(/} /g, '}\n');
-    transformed = transformed.replace(/},/g, '},\n');
-    transformed = transformed.replace(/:/g, ':\n');
-    transformed = transformed.replace(/{/g, '\t{'); 
+    // let transformed = new ScgHighlightingPipe().transform(input);
+    let transformed = this.formatExpression(input);
     return this.sanitizer.bypassSecurityTrustHtml(transformed);
   }
+
+  formatExpression(expression: string): string {
+    let formatted = expression
+    .replace(/\{/g, '\n  {')        // Newline after colon before the first attribute set (with `{}`)
+      // .replace(/: \{/g, ':\n  {')        // Newline after colon before the first attribute set (with `{}`)
+      // .replace(/, \{/g, ',\n  {')        // Newline before opening each additional attribute set (with `{}`)
+      // .replace(/\} ,/g, '},\n  ')        // Newline after closing brace and before commas (with `{}`)
+      // .replace(/\{ /g, '{\n    ')        // Newline and indent inside each attribute set (with `{}`)
+      // .replace(/\}/g, ' }')              // Keep closing brace on the same line as the last attribute (with `{}`)
+      // .replace(/: /g, ':\n  ')           // Newline after the colon for expressions without `{}` as well
+      // .replace(/, /g, ',\n  ')           // Newline before each attribute (for both types of expressions)
+      // .replace(/ = /g, ' = ');           // Ensure proper spacing around equal signs (for both types)
+
+    return formatted;
+  }
+  
 
 }
